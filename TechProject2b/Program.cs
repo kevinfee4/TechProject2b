cdc1ba724c32cfecb543b5d75d70e414c519281f
Kevin <Kevin@DESKTOP-976O6J1>
2020-09-16 17:09:02 -0400

    TechProj2b

--- TechProject2b/Program.cs
+++ TechProject2b/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Kevin Fee
+// 09/16/2020
+using System;
 
 namespace TechProject2b
 {
@@ -6,7 +8,44 @@ namespace TechProject2b
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Please enter grade number");
+
+			try
+			{
+				string input = Console.ReadLine();
+				decimal gradeNumber = decimal.Parse(input);
+
+				if (gradeNumber >= 90)
+				{
+					Console.WriteLine("A");
+					Console.ReadKey(true);
+				}
+				else if ((gradeNumber <= 89) && (gradeNumber >= 80))
+				{
+					Console.WriteLine("B");
+					Console.ReadKey(true);
+				}
+				else if ((gradeNumber <= 79) && (gradeNumber >= 70))
+				{
+					Console.WriteLine("C");
+					Console.ReadKey(true);
+				}
+				else if ((gradeNumber <= 69) && (gradeNumber >= 60))
+				{
+					Console.WriteLine("D");
+					Console.ReadKey(true);
+				}
+				else if (gradeNumber < 60)
+				{
+					Console.WriteLine("F");
+					Console.ReadKey(true);
+				}
+			}
+			catch
+			{
+				Console.WriteLine("Press any key to exit the program...");
+				Console.ReadKey(true);
+			}
 		}
 	}
 }
